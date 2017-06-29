47ed9a43448c2d189fdb9c545d6fea38473ff6fe
Joshua Greben <jgreben@stanford.edu>
2017-06-29 16:59:10 -0700

    Load profiles when application is loaded

--- builds/bfe.dev.js
+++ builds/bfe.dev.js
@@ -585,7 +585,11 @@ bfe.define('src/bfe', ['require', 'exports', 'module' , 'src/lib/jquery-2.1.0.mi
 
         $menudiv.append("<h3>Create Resource</h3>");
 
-	$(document).ready(function() {
+  $(document).onload(function() {
+  	config.loadProfiles.callback(bfelog, config.profileDir);
+	});
+
+  $(document).ready(function() {
     $('.dropdown-toggle').click(function() {
   	  config.loadProfiles.callback(bfelog, config.profileDir);
   	});
